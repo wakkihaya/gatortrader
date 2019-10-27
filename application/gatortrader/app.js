581604fc8442668fc2e297cc1fc1e5a4e18605f4
wakkihaya <wakkihaya@gmail.com>
2019-10-27 15:26:04 +0900

    display the image to the screen

--- application/gatortrader/app.js
+++ application/gatortrader/app.js
@@ -125,6 +125,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+//to display the static files(images) to the .ejs
+app.use(express.static('public'));
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
