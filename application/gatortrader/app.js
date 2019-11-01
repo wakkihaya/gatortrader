00f7d4c9654dc5f5176aa10e74dafb7b9247de0a
Anton Abramson <Anton.Abramson1@gmail.com>
2019-11-01 15:14:54 -0700

    Created new item_listing page, cleaned up sell page, added css to all pages

--- application/gatortrader/app.js
+++ application/gatortrader/app.js
@@ -1,144 +1,150 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var ejs = require('ejs');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var ejs = require("ejs");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // app.set('view engine', 'jade');
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-
-app.get('/aboutUs',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutUs.html');
+app.get("/aboutUs", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutUs.html");
 });
 
-app.get('/aboutAnmol',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutAnmol.html');
+app.get("/aboutAnmol", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutAnmol.html");
 });
 
-app.get('/aboutAnton',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutAnton.html');
+app.get("/aboutAnton", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutAnton.html");
 });
 
-app.get('/aboutDann',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutDann.html');
+app.get("/aboutDann", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutDann.html");
 });
 
-app.get('/aboutGrayson',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutGrayson.html');
+app.get("/aboutGrayson", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutGrayson.html");
 });
 
-app.get('/aboutHayato',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutHayato.html');
+app.get("/aboutHayato", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutHayato.html");
 });
 
-app.get('/aboutSamjot',(req, res)=>{
-	res.sendFile(__dirname + '/public/frontend/aboutSamjot.html');
+app.get("/aboutSamjot", (req, res) => {
+  res.sendFile(__dirname + "/public/frontend/aboutSamjot.html");
 });
 
-app.get('/',(req, res)=>{
-	res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
-
 
 // Establish connection to the database from app.js file of application
-const mysql = require('mysql');
+const mysql = require("mysql");
 const database = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'Fr0ntEndBackEnd',
-	database: 'gt_database',
+  host: "localhost",
+  user: "root",
+  password: "Fr0ntEndBackEnd",
+  database: "gt_database"
 });
 
 //connect to database
 database.connect(function(err) {
-	if (err) console.log(err);
-	console.log('Connected!');
+  if (err) console.log(err);
+  console.log("Connected!");
 });
 
-app.get('/index',function (req,res) {
-	database.query('select * from items', (err, result) => {
-		if(err) console.log(err);
-		res.render('index',{searchResult:result,results:result.length});
-	});
+app.get("/index", function(req, res) {
+  database.query("select * from items", (err, result) => {
+    if (err) console.log(err);
+    res.render("index", { searchResult: result, results: result.length });
+  });
 });
 
 function search(req, res, next) {
+  var searchTerm = req.query.search;
+  var category = req.query.category;
 
-	var searchTerm = req.query.search;
-	var category = req.query.category;
+  console.log("cate" + category);
+  console.log("search" + searchTerm);
 
-	console.log("cate" +category);
-	console.log("search"+searchTerm);
-
-	let query = 'select * from items';
-	if (searchTerm != '' && category != '') {
-		query = "select * from items where category_id = " + category + " and (item_name like '%" + searchTerm + "%' or description like '%" + searchTerm + "%')";
-	}
-	else if (searchTerm != '' && category == '') {
-		query = "select * from items where item_name like '%" + searchTerm + "%' or description like '%" + searchTerm + "%'";
-	}
-	else if (searchTerm == '' && category != '') {
-		query = 'select * from items where category_id =  ' + category;
-	}else{ // no words -> redirect to /index
-		return res.redirect('/index');
-	}
-	database.query(query, (err, result) => {
-		req.searchResult = result;
-		req.searchTerm = searchTerm;
-		req.category = category;
-		next();
-	});
+  let query = "select * from items";
+  if (searchTerm != "" && category != "") {
+    query =
+      "select * from items where category_id = " +
+      category +
+      " and (item_name like '%" +
+      searchTerm +
+      "%' or description like '%" +
+      searchTerm +
+      "%')";
+  } else if (searchTerm != "" && category == "") {
+    query =
+      "select * from items where item_name like '%" +
+      searchTerm +
+      "%' or description like '%" +
+      searchTerm +
+      "%'";
+  } else if (searchTerm == "" && category != "") {
+    query = "select * from items where category_id =  " + category;
+  } else {
+    // no words -> redirect to /index
+    return res.redirect("/index");
+  }
+  database.query(query, (err, result) => {
+    req.searchResult = result;
+    req.searchTerm = searchTerm;
+    req.category = category;
+    next();
+  });
 }
 
-app.get('/search', search, (req, res) => {
-
-	//searchResult is type of Json
-	var searchResult = req.searchResult;
-	res.render('search_result',{
-		results: searchResult.length,
-		searchTerm: req.searchTerm,
-		searchResult: searchResult,
-		category: req.category
-	});
+app.get("/search", search, (req, res) => {
+  //searchResult is type of Json
+  var searchResult = req.searchResult;
+  res.render("search_result", {
+    results: searchResult.length,
+    searchTerm: req.searchTerm,
+    searchResult: searchResult,
+    category: req.category
+  });
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 //to display the static files(images) to the .ejs
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error',{error:err});
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error", { error: err });
 });
 
 app.listen(3000);
