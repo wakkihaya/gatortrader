6e6986d05e8367f55853fbfa628631ea4bc22ab8
Daniel Gutierrez <dgutierrez1@mail.sfsu.edu>
2019-12-02 23:33:34 -0800

    Created modular router for main routes. Each route makes use of controller modules to retrieve necessary data and render views. POST new route unfinished.

---
+++ application/gatortrader/routes/gatortraderRoute.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+
+/* Require controller modules */
+var category_controller = require('../controllers/categoryController');
+var item_controller = require('../controllers/itemController');
+var gatortrader_controller = require('../controllers/gatortraderController');
+
+/* INDEX GLOBAL MIDDLEWARE */
+
+/* Logger middleware function */
+router.use(function (req, res, next) {
+  console.log('/ middleware (Logger)');
+  console.log('%s %s %s ', req.method, req.baseUrl, req.path);
+  next();
+});
+
+/* GATORTRADER GET ROUTES */
+
+/* GET home page */
+router.get('/',
+    category_controller.list_all_categories,
+    item_controller.list_all_items,
+    gatortrader_controller.render_index
+);
+router.get('/index',
+    category_controller.list_all_categories,
+    item_controller.list_all_items,
+    gatortrader_controller.render_index
+);
+
+/* GET new item page */
+router.get('/new',
+    category_controller.list_all_categories,
+    gatortrader_controller.render_new
+);
+
+/* GET signIn page */
+router.get('/signIn',
+    gatortrader_controller.render_signIn
+);
+
+/* GET registration page */
+router.get('/registration',
+    gatortrader_controller.render_registration
+);
+
+/* GET item_listing page */
+router.get('/item_listing/:item_id',
+    category_controller.list_all_categories,
+    item_controller.find_item_by_item_id,
+    gatortrader_controller.render_item_listing
+);
+
+/* GATORTRADER POST ROUTES */
+
+/* POST new item */
+router.post('/new'
+
+);
+
+module.exports = router;
