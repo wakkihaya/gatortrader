8644e97944e8032a59361f6976f9c31f78320454
Daniel Gutierrez <dgutierrez1@mail.sfsu.edu>
2019-12-02 18:05:56 -0800

    Initial commit of cleaned-up code. Raw copy of README.md. Clean app.js.

--- application/gatortrader/app.js
+++ application/gatortrader/app.js
@@ -1,250 +1,57 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var ejs = require("ejs");
-const multer = require('multer');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+var multer = require('multer');
 
-
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var aboutRouter = require('./routes/aboutRoute');
+var gatortraderRouter = require('./routes/gatortraderRoute');
+var searchRouter = require('./routes/searchRoute');
+var usersRouter = require('./routes/usersRoute');
 
 var app = express();
 
-// app.set('view engine', 'jade');
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(logger("dev"));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
-const upDir = path.join(__dirname,"public/.upload");
-const uploadDir = multer({dest: upDir});
+app.use('/about', aboutRouter);
+app.use('/search', searchRouter);
+app.use('/users', usersRouter);
+app.use('/', gatortraderRouter);
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-app.get("/aboutUs", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutUs.html");
-});
-
-app.get("/aboutAnmol", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutAnmol.html");
-});
-
-app.get("/aboutAnton", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutAnton.html");
-});
-
-app.get("/aboutDann", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutDann.html");
-});
-
-app.get("/aboutGrayson", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutGrayson.html");
-});
-
-app.get("/aboutHayato", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutHayato.html");
-});
-
-app.get("/aboutSamjot", (req, res) => {
-  res.sendFile(__dirname + "/public/frontend/aboutSamjot.html");
-});
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// Establish connection to the database from app.js file of application
-const mysql = require("mysql");
-const database = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Fr0ntEndBackEnd",
-  database: "gt_database"
-});
-
-//for validation
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: true
-}));
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
-
-
-const { check, validationResult } = require('express-validator');
-
-//connect to database
-database.connect(function(err) {
-  if (err) console.log(err);
-  console.log("Connected!");
-});
-
-//for default page with all items.
-app.get("/index", function(req, res) {
-  database.query("select * from items", (err, result) => {
-    if (err) console.log(err);
-    res.render("index", { searchResult: result, results: result.length });
-  });
-});
-
-//search function
-function search(req, res, next) {
-  var searchTerm = req.query.search;
-  var category = req.query.category;
-
-  console.log("cate" + category);
-  console.log("search" + searchTerm);
-
-  let query = "select * from items";
-  if (searchTerm != "" && category != "") {
-    query =
-      "select * from items where category_id = " +
-      category +
-      " and (item_name like '%" +
-      searchTerm +
-      "%' or description like '%" +
-      searchTerm +
-      "%')";
-  } else if (searchTerm != "" && category == "") {
-    query =
-      "select * from items where item_name like '%" +
-      searchTerm +
-      "%' or description like '%" +
-      searchTerm +
-      "%'";
-  } else if (searchTerm == "" && category != "") {
-    query = "select * from items where category_id =  " + category;
-  } else {
-    // no words -> redirect to /index
-    return res.redirect("/index");
-  }
-  database.query(query, (err, result) => {
-    req.searchResult = result;
-    req.searchTerm = searchTerm;
-    req.category = category;
-    next();
-  });
-}
-
-//display the result of search by using search function
-app.get("/search", search, (req, res) => {
-  //searchResult is type of Json
-  var searchResult = req.searchResult;
-  res.render("search_result", {
-    results: searchResult.length,
-    searchTerm: req.searchTerm,
-    searchResult: searchResult,
-    category: req.category
-  });
-});
-
-//for sell page
-app.get("/new", (req, res) => {
-  res.render("new",{ messages:''});
-});
-
-app.get("/signIn", (req, res) => {
-  res.render("signIn");
-});
-
-app.get("/registration", (req, res) => {
-  res.render("registration");
-});
-
-//for files
-// var storage = multer.diskStorage({
-//   destination: function(req, file, callback){
-//     callback(null, 'public/.upload')
-//   }
-// })
-
-//写真をフォルダに保存はできた。それをdatabase に保存するときに、変数名のせいで保存が出来ていない。
-
-//put the input values on sell page to database
-app.post("/new",uploadDir.single('itemImage'),
-    [
-      check('itemName').isLength({min: 1,max: 40}).withMessage("The length of item name is up to 40"),
-      check('itemName').matches(/^[a-z0-9 ]+$/i).withMessage("Use only numbers and Letters"),
-      check('itemCost').isNumeric().withMessage("Use only numbers"),
-      check('itemDescription').isLength({max:200}).withMessage("The length of item description is up to 200")
-    ],
-    (req, res) =>{
-  var itemName = req.body.itemName;
-  var itemCategory = req.body.category;
-  var itemCost = req.body.itemCost;
-  var itemDescription = req.body.itemDescription;
-
-  //Actually,pull user ID from Login information.
-  var userID =5;
-
-  console.log("file"+ req.files);
-
-  //about image
-      var file = req.file;
-      var itemImage = file.filename;
-
-
-  //for validation
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-      res.render("new",{messages: errors.array()});
-  }else {
-    var query = 'INSERT INTO items (item_name, description, image, category_id, price, user_id) values ("' + itemName + '",' + '"' + itemDescription + '",' + '"' + itemImage + '",' + '"' + itemCategory + '",' + '"' + itemCost + '",' + '"' + userID + '")';
-    database.query(query, function (err, rows) {
-      res.redirect('/index');
-    });
-  }
-});
-
-
-// view a single item listing
-app.get("/item_listing/:id", (req, res) => {
-  var item_id = parseInt(req.params.id) + 1;
-  console.log("item" + item_id);
-  database.query(
-    "select * from items where item_id =" + item_id,
-    (err, result) => {
-      if (err) console.log(err);
-      console.log("sr: "+ result );
-      console.log("r: "+result.length );
-      res.render("item_listing", {
-        searchResult: result,
-        results: result.length
-      });
-    }
-  );
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-//to display the static files(images) to the .ejs
-app.use(express.static("public"));
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error", { error: err });
+  res.render('error');
 });
 
-app.listen(3000);
+app.listen(3000, function (req,res) {
+  console.log('Express JS is ready for port 3000');
+});
+
 module.exports = app;
