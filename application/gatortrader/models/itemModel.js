c7d87f5f1119719b8087c1a2fa34ded837dba160
Daniel Gutierrez <dgutierrez1@mail.sfsu.edu>
2019-12-02 18:28:08 -0800

    Created modular SQL query functions for the items table in the db: [getAllItems], [getItemById], [getItemsByCategoryId], [getItemsByUserId], [getItemsBySearchTerm], and [getItemsByCategoryIdSearchTerm].

---
+++ application/gatortrader/models/itemModel.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var sql = require('./gt_db.js');
+
+// item object constructor
+var Item = function (item) {
+    this.item_id = item.item_id;
+    this.user_id = item.user_id;
+    this.category_id = item.category_id;
+    this.item_name = item.item_name;
+    this.description = item.description;
+    this.image_path = item.image_path;
+    this.price = item.price;
+    this.date_created = item.date_created;
+    this.in_stock = item.in_stock;
+};
+Item.getAllItems = function (result) {
+    console.log('itemModel');
+    sql.query('SELECT * FROM items ORDER BY date_created DESC', function (err, res) {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log(res);
+            result(null, res);
+        }
+    });
+};
+Item.getItemByItemId = function (itemId, result) {
+    console.log('itemModel');
+    sql.query('SELECT * FROM items WHERE item_id=?', itemId, function (err, res) {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log('item: ', res);
+            result(null, res);
+        }
+    });
+};
+Item.getItemsByCategoryId = function (categoryId, result) {
+    console.log('itemModel');
+    sql.query('SELECT * FROM items WHERE category_id=? ORDER BY date_created DESC', categoryId, function (err, res)  {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log('items: ', res);
+            result(null, res);
+        }
+    });
+};
+Item.getItemsByUserId = function (userId, result) {
+    console.log('itemModel');
+    sql.query('SELECT * FROM items WHERE user_id=?', userId, function (err, res)  {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log('items: ', res);
+            result(null, res);
+        }
+    });
+};
+
+Item.getItemsBySearchTerm = function (searchTerm, result) {
+    console.log('itemModel');
+    var query = "SELECT * FROM items WHERE item_name LIKE '%" +
+        searchTerm +
+        "%' OR description LIKE '%" +
+        searchTerm +
+        "%'";
+    sql.query(query, function (err, res) {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log('items: ', res);
+            result(null, res);
+        }
+    });
+};
+
+Item.getItemsByCategoryIdSearchTerm = function (categoryId, searchTerm, result) {
+    console.log('itemModel');
+    var query = "SELECT * FROM items WHERE category_id = " +
+        categoryId +
+        " AND (item_name LIKE '%" +
+        searchTerm +
+        "%' OR description LIKE '%" +
+        searchTerm +
+        "%')";
+    sql.query(query, function (err, res) {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+        }
+        else {
+            console.log('items: ', res);
+            result(null, res);
+        }
+    });
+};
+
+module.exports = Item;
