6aab994f19332529e389aa2adf2bcf1aa6207e9a
Daniel Gutierrez <dgutierrez1@mail.sfsu.edu>
2019-12-02 19:01:36 -0800

    Created modular controller functions for rendering the gatortrader views. Some require proper retrieval of categories and items from db. Some also require variables from search query.

---
+++ application/gatortrader/controllers/gatortraderController.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/* Render index view */
+exports.render_index = function (req, res) {
+    console.log('gatortraderController');
+    console.log(res.locals.categories);
+    var viewData = {
+        categories : res.locals.categories,
+        items : res.locals.items
+    };
+    res.render('pages/gatortrader/index', viewData);
+};
+
+/* Render browse view */
+exports.render_browse = function (req, res) {
+    console.log('gatortraderController');
+    console.log(res.locals.items);
+    var viewData = {
+        category_id : req.query.category,
+        searchTerm : req.query.search,
+        categories : res.locals.categories,
+        items : res.locals.items
+    };
+    res.render('pages/gatortrader/browse', viewData);
+};
+
+/* Render item_listing view */
+exports.render_item_listing = function (req, res) {
+    console.log('gatortraderController');
+    console.log(res.locals.item);
+    var viewData = {
+        category_id : req.query.category,
+        searchTerm : req.query.search,
+        categories : res.locals.categories,
+        item: res.locals.item
+    };
+    res.render('pages/gatortrader/item_listing', viewData);
+};
+
+/* Render sell view */
+exports.render_new = function (req, res) {
+    console.log('gatortraderController');
+    console.log(res.locals.categories);
+    var viewData = {
+        categories : res.locals.categories
+    };
+    res.render('pages/gatortrader/new', viewData);
+};
+
+/* Render signIn view */
+exports.render_signIn = function (req, res) {
+    console.log('gatortraderController');
+    res.render('pages/gatortrader/signIn');
+};
+
+/* Render registration view */
+exports.render_registration = function (req, res) {
+    console.log('gatortraderController');
+    res.render('pages/gatortrader/registration');
+};
